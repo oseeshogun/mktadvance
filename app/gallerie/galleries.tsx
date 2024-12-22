0fdeec0e19ed53f925f49dde02b7281ede072f10
oseeshogun <omasuaku@gmail.com>
2024-12-22 12:44:28 +0100

    feat(galleries): enhance gallery item display with captions and improved layout

--- app/gallerie/galleries.tsx
+++ app/gallerie/galleries.tsx
@@ -41,15 +41,25 @@ const GalleriesComponent = ({ items }: { items: GalleryItem[] }) => {
   return (
     <div className="min-h-screen px-[5%] py-[10%]">
       <h1 className="text-3xl font-bold">Gallerie</h1>
-      <h3 className="text-md w-1/2">Bienvenue dans notre galerie, un espace où chaque image raconte une histoire.</h3>
+      <h3 className="text-md w-1/2">
+        Bienvenue dans notre galerie, un espace où chaque image raconte une
+        histoire.
+      </h3>
       <div className="flex flex-wrap">
         {items.map((item, i) => (
           <button
             key={i}
-            className="w-1/4 p-2 group overflow-hidden"
+            className="w-1/4 p-2 group overflow-hidden relative h-fit"
             onClick={() => handleClick(i)}
           >
-            <img src={item.src} alt={item.alt} className="object-contain group-hover:scale-110 transition-all duration-300" />
+            <img
+              src={item.src}
+              alt={item.alt}
+              className="object-contain group-hover:scale-110 transition-all duration-300"
+            />
+            <div className="absolute inset-0 bg-black/30 flex items-end justify-start opacity-0 group-hover:opacity-100 transition-all duration-300 translate-y-[300%] group-hover:translate-y-0">
+              <p className="text-white text-lg text-left p-5 font-bold">{item.caption}</p>
+            </div>
           </button>
         ))}
       </div>
