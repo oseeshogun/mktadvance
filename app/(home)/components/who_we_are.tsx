fc113ed8724dc753468c96c26832c8bb63109d0d
oseeshogun <omasuaku@gmail.com>
2025-01-12 15:56:30 +0100

    update

--- app/(home)/components/who_we_are.tsx
+++ app/(home)/components/who_we_are.tsx
@@ -1,43 +1,35 @@
 "use client"
 
-import TextRevealByWord from "@/components/ui/text-reveal"
 import Image from "next/image"
 import { Slide } from "react-awesome-reveal"
 
 const WhoWeAre = () => {
   return (
-    <div className="py-20 flex max-md:flex-col-reverse relative" id="about_us">
-      <div className="w-[50%] max-md:w-full px-[3%]">
-        {/* <h2 className="text-4xl max-md:text-3xl font-bold">
-          Qui sommes-nous ?
-        </h2> */}
-        <div className="italic my-3 capitalize font-semibold [&>*]:my-2">
-          <TextRevealByWord text="Agence de Conseil en Communication Globale" />
-          <TextRevealByWord text="Conception, Production, Installation des Enseignes et Supports de Visibilité" />
-          <TextRevealByWord text="Enseignes Signalétiques Lumineuses et Non-Lumineuses" />
-          <TextRevealByWord text="Lettrages 3D Lumineux et Non-Lumineux" />
-          <TextRevealByWord text="Impression Grand Format (bâche, vinyle, one way,...)" />
-          <TextRevealByWord text="Découpe au Laser des Supports Créatifs (Plexi, Alucobond, bois,...)" />
-          <TextRevealByWord text="Affichage Publicitaire" />
+    <div className="py-20 flex max-md:flex-col-reverse" id="about_us">
+      <div className="w-[50%] max-md:w-full px-[5%]">
+        <div className="italic my-3 text-3xl capitalize font-semibold flex flex-col [&>*]:my-5">
+          <p>AGENCE DE CONSEIL EN COMMUNICATION GLOBALE</p>
+          <p>
+            CONCEPTION PRODUCTION, INSTALLATION DES ENSEIGNES ET SUPPORTS DE
+            VISIBILITE
+          </p>
+          <p>ENSEIGNES SIGNALETIQUES LUMINEUSES ET NON-LUMINEUSES </p>
+          <p> LETTRAGES 3D LUMINEUX ET NON-LUMINEUX</p>
+          <p>IMPRESSION GRAND FORMAT (Bâche, Vinyle, One Way,...)</p>
+          <p>
+            DECOUPE AU LASER DES SUPPORTS CREATIFS (Plexi, Alucobonde, bois,...)
+          </p>
+          <p>AFFICHAGE PUBLICITAIRE</p>
         </div>
-        {/* <p className="mt-6 text-lg font-semibold">
-          Nous croyons en la puissance de l&apos;innovation et de la simplicité.
-          Chaque projet est une opportunité de transformer une idée en réalité,
-          en repoussant les limites de ce qui est possible.
-        </p> */}
       </div>
-      <div className="w-[50%] max-md:w-full max-md:mb-4 px-[5%] relative">
-        <Slide
-          direction="right"
-          triggerOnce
-          className="sticky top-[20%] right-0"
-        >
+      <div className="w-[50%] max-md:w-full max-md:mb-4 px-[5%]">
+        <Slide direction="right" triggerOnce>
           <Image
             src="/assets/images/Nous (2) (1).png"
             alt="Madame Mireille Kaimby Tshiyoyo"
             height={239}
             width={239}
-            className="w-full object-cover slide-reveal-right transition-all duration-300 sticky top-0 right-0"
+            className="w-full object-cover slide-reveal-right transition-all duration-300"
             quality={100}
             unoptimized
           />
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import type { PortableTextBlock } from "@portabletext/react"
+import type { PortableTextSpan } from "sanity"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const slugify = (text: string) => {
+  if (!text) {
+    // generate random text
+    const randomText = Math.random().toString(36).slice(2)
+    return randomText
+  }
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+}
+
+export const extractTextFromPortableTextBlock = (
+  block: PortableTextBlock,
+): string => {
+  return block.children
+    .filter(
+      (child): child is PortableTextSpan =>
+        typeof child === "object" && "_type" in child && "text" in child,
+    )
+    .map((child) => child.text)
+    .join("")
 }
